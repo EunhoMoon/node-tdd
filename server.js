e5567cb0894bac26510f78b267e002efaf00259c
Janek <eunho.moon@gmail.com>
2023-04-27 23:42:47 +0900

    jest 세팅

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = 3000;
+
+const app = express();
+
+const productRouter = require('./router');
+
+
+mongoose.connect('mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(result => {
+    console.log('mongodb connected');
+}).catch(error => {
+    console.log(error);
+});
+
+app.use(express.json());
+
+app.use('/', productRouter);
+
+app.listen(PORT);
+console.log(`Running on port ${PORT} 🐸`);

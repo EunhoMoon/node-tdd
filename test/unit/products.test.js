9cc30b08644bb5e8e1841d8460bb52b34cb86176
Janek <eunho.moon@gmail.com>
2023-04-29 23:40:15 +0900

    create unit test

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,14 +1,38 @@
 const productController = require('../../controller/products');
 const productModel = require('../../models/Product');
+const httpMocks = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
 
 productModel.create = jest.fn();
 
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
 describe('Product Controller Create', () => {
+    beforeEach(() => {
+        req.body = newProduct;
+    });
+
     it('Product 객체 생성은 함수여야 한다.', () => {
         expect(typeof productController.createProduct).toBe('function');
     });
     it('productModel.create()가 호출되어야 한다.', () => {
-        productController.createProduct();
-        expect(productModel.create).toBeCalled();
+        productController.createProduct(req, res, next);
+        expect(productModel.create).toBeCalledWith(newProduct);
+    });
+    it('성공시 상태 코드 201을 반환한다.', () => {
+        productController.createProduct(req, res, next);
+        expect(res.statusCode).toBe(201);
+        expect(res._isEndCalled()).toBeTruthy();
+    })
+    it('성공시 응답에 json body가 포함되어있다. ',  () => {
+        productModel.create.mockReturnValue(newProduct);
+        productController.createProduct(req, res, next);
+        expect(res._getJSONData()).toStrictEqual(newProduct);
     });
 });

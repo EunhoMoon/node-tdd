df2705bd47b24acbebd314ebf575ef2236361106
Janek <eunho.moon@gmail.com>
2023-05-01 00:11:03 +0900

    create integration test

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,9 +1,12 @@
 const productController = require('../../controller/products');
 const productModel = require('../../models/Product');
 const httpMocks = require('node-mocks-http');
+
 const newProduct = require('../data/new-product.json');
+const allProducts = require('../data/all-products.json');
 
 productModel.create = jest.fn();
+productModel.find = jest.fn();
 
 let req, res, next;
 
@@ -43,5 +46,25 @@ describe('Product Controller Create', () => {
         productModel.create.mockReturnValue(rejectedPromise);
         await productController.createProduct(req, res, next);
         expect(next).toBeCalledWith(errorMessage);
+    });
+});
+
+describe('Product Controller Get', () => {
+    it('productController.getProducts 호출시 그 type은 함수여야 한다.',  () => {
+        expect(typeof productController.getProducts).toBe('function');
+    });
+    it('ProductModel.find({}) 호출 성공', async () => {
+        await productController.getProducts(req, res, next);
+        expect(productModel.find).toHaveBeenCalledWith({});
+    });
+    it('성공시 상태코드 200을 리턴', async () => {
+        await productController.getProducts(req, res, next);
+        expect(res.statusCode).toBe(200);
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+    it('json 객체를 응답', async () => {
+        productModel.find.mockReturnValue(allProducts);
+        await productController.getProducts(req, res, next);
+        expect(res._getJSONData()).toStrictEqual(allProducts);
     });
 });
